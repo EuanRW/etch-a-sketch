8cf33c6d50bc22da7e202e9f010d3876dc0393aa
Euan Williams  [Rapid DCS] <euanwilliams00@gmail.com>
2022-09-10 14:24:52 +0100

    Adding clear canvas functionality.

--- script.js
+++ script.js
@@ -7,36 +7,42 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
     output.innerHTML = this.value;
-    sliderVal = this.value
-    makeRows(sliderVal, sliderVal)
+    sliderVal = this.value;
+    makeRows(sliderVal, sliderVal);
 };
+
+//CLEAR BUTTON
+const clearBtn = document.getElementById("clearBtn");
+
+clearBtn.addEventListener("click", () => {
+    const allCells = document.querySelectorAll(".my-color-class");
+    allCells.forEach(function (el) {
+        el.classList.remove("my-color-class");
+    });
+});
 
 //GRID
 const grid = document.getElementById("grid");
 
-function removeElementsByClass(className){
-    console.log("removeElementsByClass fired!")
+function removeElementsByClass(className) {
     const elements = document.getElementsByClassName(className);
-    while(elements.length > 0){
+    while (elements.length > 0) {
         elements[0].parentNode.removeChild(elements[0]);
     }
 }
 
 function makeRows(rows, cols) {
-    removeElementsByClass("grid-item")
+    removeElementsByClass("grid-item");
     grid.style.setProperty("--grid-rows", rows);
     grid.style.setProperty("--grid-cols", cols);
     for (c = 0; c < rows * cols; c++) {
         let cell = document.createElement("div");
-        cell.innerText = c + 1;
+        // cell.innerText = c + 1;
         grid.appendChild(cell).className = "grid-item";
+        cell.addEventListener("mouseover", (e) =>
+            e.target.classList.add("my-color-class")
+        );
     }
 }
 
 makeRows(sliderVal, sliderVal);
-
-//Delete button
-const deleteButton = document.getElementById("delete");
-
-deleteButton.addEventListener("click", () => removeElementsByClass("grid-item"));
-
